d0d793c3e87de996bd268f861b89d7efeddc4935
130347665 <benson901203@yahoo.com.tw>
2022-06-16 21:19:46 +0800

    Initial commit

--- Auto_sport_ui.py
+++ Auto_sport_ui.py
@@ -1,4 +1,6 @@
 import sys
+from PyQt5 import sip
+import PyQt5.QtWidgets
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from GUI.Sport import *
 from PyQt5.QtCore import QThread, pyqtSignal,QDateTime
@@ -14,6 +16,18 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import random
 import os
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import datetime
+
+nowtime = datetime.date.today()
+second_date = datetime.date(2022, 6, 18)
+if nowtime < second_date:
+    print("可以用")
+else:
+    quit()
+print(nowtime)
 #大漠初始化
 dm = win32com.client.Dispatch('dm.dmsoft')
 print(dm.ver())
@@ -36,6 +50,16 @@ class BackQthread(QThread):
     sport_league_name = pyqtSignal(str)
     #當前循環為int參數類型
     now_loop = pyqtSignal(str)
+    #下注場次名稱為str參數類型
+    betting_name = pyqtSignal(str)
+    # 以下注過的信息_標題為str參數類型
+    betting_title = pyqtSignal(str)
+    # 以下注過的信息_下注隊伍為str參數類型
+    betting_team = pyqtSignal(str)
+    # 以下注過的信息_隊伍信息為str參數類型
+    betting_team_inf = pyqtSignal(str)
+    # 以下注過的信息_list信息為list參數類型
+    betting_inf = pyqtSignal(list)
     def __init__(self,parent1,parent2,parent3,account,password):
         super(BackQthread, self).__init__()
         self.parent1 = parent1
@@ -44,6 +68,7 @@ class BackQthread(QThread):
         self.account = account
         self.password = password
     def run(self):
+        以下注過的信息 = []
         for num in range(0,self.parent1):
             當前循環 = num+1
             self.now_loop.emit(str(當前循環))
@@ -70,7 +95,8 @@ class BackQthread(QThread):
             driver.get(sportWebsite)
             driver.implicitly_wait(6)  # 等待加載完成 最多6秒
             #拿到當前餘額 '//*[@id="use-credit-15"]'
-            now_money = driver.find_element(By.XPATH, '//*[@id="use-credit-15"]').text
+            tonow = datetime.datetime.now()
+            now_money = driver.find_element(By.XPATH, '//*[@id="use-credit-{0}"]'.format(tonow.day+1)).text
             print(now_money)
             self.current_balance.emit(now_money)
 
@@ -109,6 +135,82 @@ class BackQthread(QThread):
                 exit()
             下注按鈕總數[隨機數].click()  ##隨機點擊一個下注
 
+            #0616
+
+            while True:
+                下注場次名稱 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[2]/div/div[1]').text
+                print(下注場次名稱)
+                以下注過的信息_標題 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[1]').text
+                print(以下注過的信息_標題)
+                以下注過的信息_下注隊伍 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[2]/div/div[3]').text
+                print(以下注過的信息_下注隊伍)
+                以下注過的信息_隊伍信息 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[2]/div/div[2]').get_attribute(
+                    'innerText')
+                for i in range(0, len(以下注過的信息)):
+                    if 以下注過的信息_標題 == 以下注過的信息[i][0]:
+                        if 下注場次名稱 == 以下注過的信息[i][1]:
+                            if 以下注過的信息_隊伍信息 == 以下注過的信息[i][3]:
+                                print("已經下過了 即將結束")
+                                driver.get(sportWebsite)
+                                driver.implicitly_wait(6)  # 等待加載完成 最多6秒
+                                test1 = driver.find_elements(By.XPATH,
+                                                             '//*[@id="left-menu"]/ul/li[@class=""]')  # li下 不等於hide的元素
+                                print(test1)
+                                '//*[@id="gc-1"]'
+                                for i, good in enumerate(test1):
+                                    體育賽事.append([good.text.splitlines()[0], good.get_attribute("id"),
+                                                 good.text.splitlines()[1]])  # 賽事名稱-編號-
+                                    print(體育賽事)
+                                print(體育賽事)
+                                隨機選擇體育賽事 = 體育賽事[random.randint(0, len(體育賽事) - 1)]
+                                賽事名稱 = 隨機選擇體育賽事[0]
+                                print(賽事名稱)
+                                賽事編號 = 隨機選擇體育賽事[1]
+                                driver.find_element(By.XPATH, '//*[@id="{0}"]/div'.format(賽事編號)).click()  # 隨機點選賽事
+
+                                # ''下注類型''
+                                # ''//*[@id="top-bar"]/div[2]/div[@class="sub-btn " and not(contains(text(),"過關"))]/font[text() != "(0)"]''
+                                # 選擇sunbtn 和 不包含過關的字 and <font>的text 不為0
+                                要下注什麼場 = driver.find_elements(By.XPATH,
+                                                              '//*[@id="top-bar"]/div[2]/div[contains(@class,"sub") and not(contains(text(),"過關"))]/font[text() != "(0)"]')
+                                要下注什麼場總共按鈕 = len(要下注什麼場)
+                                隨機下注場次按鈕遍號 = random.randint(1, 要下注什麼場總共按鈕)
+                                要下注什麼場[隨機下注場次按鈕遍號 - 1].click()
+                                # driver.find_element(By.XPATH, '//*[@id="top-bar"]/div[2]/div[{0}]'.format(隨機下注場次按鈕遍號)).click()
+                                '''點擊下注按鈕'''
+                                # odds
+                                '//*[@id="events-div"]/table/tbody/tr[3]/td[3]/div[3]/div[1]/a'
+                                '//*[@id="events-div"]/table/tbody/tr[3]/td[5]/div/div[2]/a'
+                                time.sleep(1)  # 休息3秒
+                                下注按鈕總數 = driver.find_elements(By.CLASS_NAME, 'odds')
+                                print("總共有{0}個按鈕可以下注".format(len(下注按鈕總數)))
+                                try:
+                                    隨機數 = random.randint(0, len(下注按鈕總數) - 1)
+                                except ValueError as e:
+                                    print("目前無賽事可以下")
+                                    # 重新選擇賽事
+                                    driver.get(website)
+                                下注按鈕總數[隨機數].click()  ##隨機點擊一個下注
+                else:
+                    print("成功跳出 沒有重複隊伍")
+                    下注場次名稱 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[2]/div/div[1]').text
+                    print(下注場次名稱)
+                    以下注過的信息_標題 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[1]').text
+                    print(以下注過的信息_標題)
+                    以下注過的信息_下注隊伍 = driver.find_element(By.XPATH, '//*[@id="betting-div"]/div[2]/div/div[3]').text
+                    print(以下注過的信息_下注隊伍)
+                    以下注過的信息_隊伍信息 = driver.find_element(By.XPATH,
+                                                       '//*[@id="betting-div"]/div[2]/div/div[2]').get_attribute(
+                        'innerText')
+                    break
+            以下注過的信息.append([以下注過的信息_標題, 下注場次名稱, 以下注過的信息_下注隊伍, 以下注過的信息_隊伍信息])
+            print(以下注過的信息)
+            #回傳給tablewidge更新ui
+            self.betting_inf.emit([以下注過的信息[num][0],以下注過的信息[num][1],以下注過的信息[num][2],以下注過的信息[num][3]])
+            # self.betting_title.emit(以下注過的信息[num][0])
+            # self.betting_name.emit(以下注過的信息[num][1])
+            # self.betting_team.emit(以下注過的信息[num][2])
+            # self.betting_team_inf.emit(以下注過的信息[num][3])
             #下注場次名稱取得
             #//*[@id="betting-div"]/div[2]/div/div[1]
             下注場次名稱 = driver.find_element(By.XPATH,'//*[@id="betting-div"]/div[2]/div/div[1]').text
@@ -124,8 +226,16 @@ class BackQthread(QThread):
             driver.find_element(By.XPATH, '//*[@id="betting-confirm"]').click()  # 點擊下注\
             # 截圖下注成功
             # class ="content"
-            time.sleep(10)
-            driver.find_element(By.CLASS_NAME, 'content').screenshot(r'abdc.png')
+            try:
+                element = WebDriverWait(driver, 20).until(
+                    EC.text_to_be_present_in_element((By.XPATH, '//*[@id="result-message"]'), "下注成功!")
+                )
+            except TimeoutException as e:
+                print("下注失敗 即將關閉")
+                driver.quit()
+            finally:
+                print('下注成功')
+                driver.find_element(By.CLASS_NAME, 'content').screenshot(r'abdc.png')
 
             time.sleep(1)
 
@@ -162,12 +272,53 @@ class BackQthread(QThread):
                     dm.SetWindowState(LineWindow, 9)
                 else:
                     print("綁定失敗")
-                dm.UnBindWindow()
+
                 time.sleep(1)
 
                 # 綁定開啟窗口
                 # ComboBoxEx32
                 Line_Select_file = dm.FindWindow("#32770", "")
+                if Line_Select_file ==0:
+                    dm.SetWindowState(LineWindow, 1)
+                    dm.SetWindowState(LineWindow, 8)
+                    dm.SetWindowState(LineWindow, 7)
+                    dm.SetWindowState(LineWindow, 12)
+                    time.sleep(0.5)
+                    dm.MoveTo(423, 615)  # 點及輸入訊息
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+                    dm.LeftDoubleClick()
+                    time.sleep(0.1)
+
+                    dm.MoveTo(409, 682)
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    dm.LeftDoubleClick()
+                    time.sleep(2)
+                    dm.SetWindowState(LineWindow, 9)
+                    print("進來了")
+                else:
+                    print("綁定失敗")
                 print(Line_Select_file)
                 time.sleep(0.2)
                 Line_Select_file_Edit = dm.FindWindowEx(Line_Select_file, "ComboBoxEx32", "")
@@ -178,6 +329,7 @@ class BackQthread(QThread):
                 Line_Select_file_Button = dm.FindWindowEx(Line_Select_file, "Button", "")
                 print("Edit路徑句柄:" + str(Line_Select_file_Edit))
                 # 發送文字
+                LineBinding = dm.UnBindWindow()
                 dm.SendString2(Line_Select_file_Edit, os.path.dirname(os.path.abspath(__file__)) + "\\" + "abdc.png")
                 # 案開啟
                 time.sleep(0.5)
@@ -208,6 +360,12 @@ class MyMainWindow(QMainWindow, Ui_Form):
         self.backend.sport_name.connect(self.set_sport_name_label)
         self.backend.sport_league_name.connect(self.set_sport_league_name_label)
         self.backend.now_loop.connect(self.set_now_loop_label)
+        #tablewidge更新UI
+        # self.backend.betting_title.connect(self.update_tablewidget_ui)
+        # self.backend.betting_name.connect(self.update_tablewidget_ui)
+        # self.backend.betting_team.connect(self.update_tablewidget_ui)
+        # self.backend.betting_team_inf.connect(self.update_tablewidget_ui)
+        self.backend.betting_inf.connect(self.update_tablewidget_ui)
         # 多线程开始
         self.backend.start()
 
@@ -231,6 +389,24 @@ class MyMainWindow(QMainWindow, Ui_Form):
         self.label_11.setText(str(loop_num))
     def set_current_balance(self, current_balance):
         self.label_2.setText(current_balance)
+    def update_tablewidget_ui(self,list):
+        row = self.tableWidget.rowCount()
+
+        self.tableWidget.insertRow(row)
+
+        betting_title = PyQt5.QtWidgets.QTableWidgetItem(list[0])
+        #item_id.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)  # 设置物件的状态为只可被选择（未设置可编辑）
+        betting_name = PyQt5.QtWidgets.QTableWidgetItem(list[1])  # 我们要求它可以修改，所以使用默认的状态即可
+
+        betting_team = PyQt5.QtWidgets.QTableWidgetItem(list[2])
+        betting_inf = PyQt5.QtWidgets.QTableWidgetItem(list[3])
+        #item_pos.setFlags(Qt.ItemIsSelectable | Qt.ItemIsEnabled)  # 设置物件的状态为只可被选择
+
+        self.tableWidget.setItem(row, 0, betting_title)
+        self.tableWidget.setItem(row, 1, betting_name)
+        self.tableWidget.setItem(row, 2, betting_team)
+        self.tableWidget.setItem(row, 3, betting_inf)
+        # 以下可以加入保存数据到数据的操作
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- Auto_sport_ui.spec
+++ Auto_sport_ui.spec
@@ -1,36 +1,44 @@
 # -*- mode: python ; coding: utf-8 -*-
 
+
 block_cipher = None
 
 
-a = Analysis(['Auto_sport_ui.py'],
-             pathex=['C:\\Users\\user\\PycharmProjects\\Sport_Auto_gambling_bet',
-                     'C:\\Users\\user\\PycharmProjects\\Sport_Auto_gambling_bet\\venv\\Lib\\site-packages\\PyQt5',
-                     'C:\\Users\\user\\PycharmProjects\\Sport_Auto_gambling_bet\\venv\\Lib\\site-packages\\PyQt5\\Qt5\\bin',
-                     'C:\\Users\\user\\PycharmProjects\\Sport_Auto_gambling_bet\\venv\\Lib\\site-packages\\PyQt5\\Qt5\\plugins'],
-             binaries=[],
-             datas=[],
-             hiddenimports=[],
-             hookspath=[],
-             runtime_hooks=[],
-             excludes=[],
-             win_no_prefer_redirects=False,
-             win_private_assemblies=False,
-             cipher=block_cipher,
-             noarchive=False)
-pyz = PYZ(a.pure, a.zipped_data,
-             cipher=block_cipher)
-exe = EXE(pyz,
-          a.scripts,
-          a.binaries,
-          a.zipfiles,
-          a.datas,
-          [],
-          name='Auto_sport_ui',
-          debug=False,
-          bootloader_ignore_signals=False,
-          strip=False,
-          upx=True,
-          upx_exclude=[],
-          runtime_tmpdir=None,
-          console=True )
+a = Analysis(
+    ['Auto_sport_ui.py'],
+    pathex=[],
+    binaries=[],
+    datas=[],
+    hiddenimports=[],
+    hookspath=[],
+    hooksconfig={},
+    runtime_hooks=[],
+    excludes=[],
+    win_no_prefer_redirects=False,
+    win_private_assemblies=False,
+    cipher=block_cipher,
+    noarchive=False,
+)
+pyz = PYZ(a.pure, a.zipped_data, cipher=block_cipher)
+
+exe = EXE(
+    pyz,
+    a.scripts,
+    a.binaries,
+    a.zipfiles,
+    a.datas,
+    [],
+    name='Auto_sport_ui',
+    debug=False,
+    bootloader_ignore_signals=False,
+    strip=False,
+    upx=True,
+    upx_exclude=[],
+    runtime_tmpdir=None,
+    console=False,
+    disable_windowed_traceback=False,
+    argv_emulation=False,
+    target_arch=None,
+    codesign_identity=None,
+    entitlements_file=None,
+)
